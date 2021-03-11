be89d93064103e2cd911b9846e7658eabee3ee99
CaioKeidi <caiokeidi@gmail.com>
2021-03-11 12:12:57 -0300

    feat: model Aluno

---
+++ API/escoladanca/aluno/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Aluno(models.Model):
+    primeiroNome = models.CharField(max_length=20)
+    sobrenome = models.CharField(max_length=80)
+    dataNascimento = models.DateField()
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.primeiroNome
+
--- API/escoladanca/escoladanca/settings.py
+++ API/escoladanca/escoladanca/settings.py
@@ -34,6 +34,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'escola',
+    'aluno',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -106,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
