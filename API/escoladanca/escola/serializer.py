0249a3f86901de542d94e8be12227c41dc5a7979
CaioKeidi <caiokeidi@gmail.com>
2021-03-12 20:18:44 -0300

    feat: add new fields to InscricoesDoAlunoSerializer

--- API/escoladanca/escola/serializer.py
+++ API/escoladanca/escola/serializer.py
@@ -17,8 +17,26 @@ class InscricaoSerializer(serializers.ModelSerializer):
         fields= "__all__"
 
 class InscricoesDoAlunoSerializer(serializers.ModelSerializer):
-    curso_nome = serializers.ReadOnlyField(source='aula.curso.nome')
+    curso = serializers.ReadOnlyField(source='aula.curso.nome')
+    aula_diaSemana = serializers.SerializerMethodField()
+    horario = serializers.ReadOnlyField(source='aula.horario')
+    professor = serializers.ReadOnlyField(source='aula.professor.apelido')
 
     class Meta:
         model = Inscricao
-        fields = ['curso_nome']
+        fields = ['curso','aula_diaSemana', 'horario', 'professor']
+    
+    def get_aula_diaSemana(self, obj):
+        return obj.aula.get_diaSemana_display() 
+
+    
+
+
+class AlunosMatriculadosEmCursoSerializer(serializers.ModelSerializer):
+    aluno_id = serializers.ReadOnlyField(source='aluno.id')
+    aluno_nome = serializers.ReadOnlyField(source='aluno.primeiroNome')
+    aluno_sobrenome = serializers.ReadOnlyField(source='aluno.sobrenome')
+
+    class Meta:
+        model = Inscricao
+        fields = ['aluno_nome','aluno_id', 'aluno_sobrenome']
